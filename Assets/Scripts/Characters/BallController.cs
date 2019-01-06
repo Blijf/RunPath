53ea18952efd7ebe914c052ba3d279a4129e23a9
Jose Escribano vega <msi_w@hotmail.com>
2019-01-06 13:16:12 +0100

    V0.0.1 Movimiento bola (sin movimiento del jugador)

--- Assets/Scripts/Characters/BallController.cs
+++ Assets/Scripts/Characters/BallController.cs
@@ -8,8 +8,8 @@ public class BallController : MonoBehaviour
 //------------------------------------------------------------
 //						VARIABLES
 //------------------------------------------------------------
-    public float speedUp;            
-    public float speedHorizontal;             
+    public float speed;         
+	public enum direction {Up, Down,Left,Right}  
     private Rigidbody2D rb2d;       
 	private float moveHorizontal;
 	Vector2 inDirection, inNormal,outDirection; //fijarme en el esquema de los vectores de colisión 
@@ -22,6 +22,8 @@ public class BallController : MonoBehaviour
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D> ();
+		//Dirección del player al iniciar la partida
+		inDirection.Set(0,1);
 	}
 	
 
@@ -44,15 +46,15 @@ public class BallController : MonoBehaviour
 		inNormal=other.contacts[0].normal;
 		outDirection= Vector2.Reflect(inDirection,inNormal);
 		inDirection=outDirection;
-		speedUp=-speedUp;
+	
+		Debug.Log(inDirection.normalized);
+
 	}
 //------------------------------------------------------------
 //						METHODS
 //------------------------------------------------------------
 	void move()
 	{
-		inDirection.Set(inDirection.x,speedUp*Time.fixedDeltaTime);
-		// inDirection=inDirection.normalized*speedUp*Time.fixedDeltaTime;
-		rb2d.AddForce(inDirection);
+		rb2d.velocity= inDirection.normalized*speed*Time.deltaTime;
 	}
 }
