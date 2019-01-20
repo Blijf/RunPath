efaad32f7e17f2c192f4590db36b09e21aaf63af
Jose Escribano vega <msi_w@hotmail.com>
2019-01-20 22:55:35 +0100

    V 0.0.8 puerta botones

---
+++ Assets/Scripts/ElementsBehavior/ButtonActivation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivation : MonoBehaviour
+{
+    public GameObject doorToOpen;//puerta que se quiere abrir
+    Collider2D collider2D;
+    Animator animator;
+    void Start()
+    {
+        collider2D= doorToOpen.GetComponent<Collider2D>();
+        animator= doorToOpen.GetComponent<Animator>();
+        animator.enabled=false;
+        collider2D.isTrigger=false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+          animator.enabled=true;
+          collider2D.isTrigger=true;
+            
+        }
+    }
+}
